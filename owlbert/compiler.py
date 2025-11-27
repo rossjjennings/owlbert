a1b5bf1c8919a4ce20da7d71642ef81402f24ab2
Ross Jennings <rossjjennings@gmail.com>
2025-11-26 21:00:00 -0500

    Proper package structure; use Sympy

---
+++ owlbert/compiler.py
@@ -0,0 +1,81 @@
+from lark import Tree, Token
+from sympy import Symbol, Integer, Float
+from operator import mul
+from functools import reduce
+
+def get_symbols(tree):
+    """
+    Get a dictionary of sympy Symbol objects for each CNAME token
+    in the input tree.
+    """
+    def is_cname(node):
+        return isinstance(node, Token) and node.type == 'CNAME'
+
+    symbols = {}
+    for token in tree.scan_values(is_cname):
+        if not token.value in symbols:
+            symbols[token.value] = Symbol(token.value)
+
+    return symbols
+
+def compile_expression(tree):
+    if isinstance(token := tree, Token):
+        if token.type == 'CNAME':
+            return Symbol(token.value)
+        elif token.type == 'NUMBER':
+            try:
+                return Integer(token.value)
+            except ValueError:
+                return Float(token.value)
+        else:
+            raise ValueError(f"unrecognized token type '{token.type}'")
+    elif isinstance(tree, Tree):
+        if tree.data == 'expression':
+            terms = []
+            negate_child = False
+            for child in tree.children:
+                if isinstance(child, Tree) and child.data == 'plus':
+                    negate_child = False
+                elif isinstance(child, Tree) and child.data == 'minus':
+                    negate_child = True
+                else:
+                    child_value = compile_expression(child)
+                    if negate_child:
+                        child_value = -child_value
+                    terms.append(child_value)
+            return sum(terms)
+        elif tree.data == 'term':
+            factors = []
+            invert_child = False
+            for child in tree.children:
+                if isinstance(child, Tree) and child.data == 'times':
+                    invert_child = False
+                elif isinstance(child, Tree) and child.data == 'divided_by':
+                    invert_child = True
+                else:
+                    child_value = compile_expression(child)
+                    if invert_child:
+                        child_value = 1/child_value
+                    factors.append(child_value)
+            return reduce(mul, factors)
+        elif tree.data == 'factor':
+            negate_child = False
+            first_child = tree.children[0]
+            if isinstance(first_child, Tree) and first_child.data == 'plus':
+                negate_child = False
+            elif isinstance(first_child, Tree) and first_child.data == 'minus':
+                negate_child = True
+            last_child = tree.children[-1]
+            child_value = compile_expression(last_child)
+            if negate_child:
+                child_value = -child_value
+            return child_value
+        elif tree.data == 'exponential':
+            first_child = tree.children[0]
+            base_value = compile_expression(first_child)
+            if len(tree.children) > 1:
+                last_child = tree.children[-1]
+                exp_value = compile_expression(last_child)
+                return base_value**exp_value
+            else:
+                return base_value
---
+++ owlbert/owlbert.py
@@ -0,0 +1,54 @@
+from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit import PromptSession, HTML
+from prompt_toolkit import print_formatted_text as print_formatted
+from lark import Lark, UnexpectedInput
+from pathlib import Path
+import sympy
+
+from .compiler import compile_expression
+
+kb = KeyBindings()
+with open(Path(__file__).parent / "grammar.lark") as f:
+    grammar = Lark(f.read(), parser='lalr')
+
+@kb.add('c-j')
+def add_line(event):
+    event.current_buffer.insert_text('\n')
+
+@kb.add('c-m')
+def accept_input(event):
+    event.current_buffer.validate_and_handle()
+
+def run_repl():
+    session = PromptSession()
+    print()
+
+    i = 1
+    while True:
+        try:
+            input_text = session.prompt(
+                HTML(f"<ansibrightgreen>In [<b>{i}</b>]:</ansibrightgreen> "),
+                key_bindings=kb,
+                multiline=True,
+            )
+        except KeyboardInterrupt:
+            continue
+        except EOFError:
+            break
+        else:
+            try:
+                tree = grammar.parse(input_text)
+            except UnexpectedInput as err:
+                result = f"Got unexpected input while parsing:\n{err}"
+            else:
+                result = sympy.expand(compile_expression(tree))
+
+            result_str = str(result)
+            if isinstance(result, sympy.Float):
+                if '.' in result_str:
+                    result_str = result_str.rstrip("0")
+            print(result_str + '\n')
+            i += 1
+
+if __name__ == "__main__":
+    run_repl()
